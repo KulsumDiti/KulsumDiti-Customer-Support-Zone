688076d864c6734417c6d6af09a9642cee041261
KulsumDiti <kulsumditi3@gmail.com>
2026-01-20 14:40:47 +0600

    Navbar add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,6 +27,34 @@ const Navbar = () => {
             <a>Contact</a>
           </li>
         </ul>
+        <div className="dropdown md:hidden mx-2">
+          <div tabIndex={0} role="button" className="btn w-28 h-11">
+            Menu
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-40  p-2 shadow-sm"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+            <li>
+              <a>Changelog</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+            <li>
+              <a>Download</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
         <button className="btn text-xs md:text-[17px] w-28 md:w-36 h-11 bg-gradient-to-r from-[#632EE3]  to-[#9F62F2] text-white border-none">
           + New Ticket
         </button>
